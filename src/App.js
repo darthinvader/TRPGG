fbad515eca30cecdf00bff442486ced0d65566e4
darthinvader <psaradakisj@gmail.com>
2021-07-15 22:40:30 +0300

    [feat] download json from google docs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import BookManager from "./Books/BooksManager";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>My App</p>
+        <BookManager />
       </header>
     </div>
   );
---
+++ src/Books/BooksManager.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BooksManager = (props) => {
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    const { REACT_APP_BOOK_JSON } = process.env;
+    axios.get(REACT_APP_BOOK_JSON).then((response) => {
+      setBooks(response.data);
+    });
+  }, []);
+  console.log(books);
+  return <div>Hello</div>;
+};
+
+export default BooksManager;
