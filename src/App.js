efaf58748f3b002e1b233c775061d89c0a92f8a4
darthinvader <psaradakisj@gmail.com>
2021-07-17 16:04:57 +0300

    [feat] added Link for books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import BooksManager from "./Books/BooksManager";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route exact path="/">
+            <Link to="/books/book">Books</Link>
+          </Route>
           <Route path="/books/book">
             <BooksManager />
           </Route>
