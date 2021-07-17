c30759acbdb4c06654a07915f57cd30d7a71ad91
darthinvader <psaradakisj@gmail.com>
2021-07-17 18:24:22 +0300

    [refactor] removed unecessary css rule

--- src/Books/BooksPreloader/BookCardPreloader.js
+++ src/Books/BooksPreloader/BookCardPreloader.js
@@ -2,7 +2,7 @@ import styles from "./BookCardPreloader.module.scss";
 
 const BookCardPreloader = () => {
   return (
-    <div className={styles.BookCard + " " + styles.BookCardPreloader}>
+    <div className={styles.BookCard}>
       <div className={styles["BookCardPreloader__img--wrapper"]}>
         <div className={styles["BookCardPreloader__img--aspect-ratio-wrapper"]}>
           <div className={styles.BookCardPreloader__img}></div>
