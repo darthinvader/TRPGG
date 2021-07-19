6552c8479028c4faddf9cc46e238ea635956d495
darthinvader <psaradakisj@gmail.com>
2021-07-19 20:02:08 +0300

    [feat] Modal class completed

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,3 +1,12 @@
-const Modal = (props) => {};
+import ModalOverlay from "./ModalOverlay/ModalOverlay";
+import styles from "./Modal.module.css";
+
+const Modal = ({ children }) => {
+  return (
+    <ModalOverlay>
+      <div className={styles.Modal}>{children}</div>
+    </ModalOverlay>
+  );
+};
 
 export default Modal;
