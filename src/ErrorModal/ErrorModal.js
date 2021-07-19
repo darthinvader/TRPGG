212c411b74fc3eba307d5dacd011b2042613a5cf
darthinvader <psaradakisj@gmail.com>
2021-07-19 20:13:03 +0300

    [fix] ErrorModal correct stylin

--- src/ErrorModal/ErrorModal.js
+++ src/ErrorModal/ErrorModal.js
@@ -1,22 +1,22 @@
 import Modal from "../Modal/Modal";
-
+import styles from "./ErrorModal.module.scss";
 const ErrorModal = ({ onClose, onRetry, Message, Title }) => {
   return (
     <Modal>
-      <button class="Modal__ClosingButton">Close</button>
+      <button className={styles.Modal__closeButton}>Close</button>
       <img
-        className="Modal__img"
+        className={styles.Modal__img}
         src="https://i.imgur.com/oNNEJE4.png"
         title="source: imgur.com"
         alt="Guess I'll Die!"
       />
-      <h1 class="Title">Error</h1>
-      <h2 class="Title">Books Didn't Load</h2>
-      <p>
+      <h1 className={styles.Modal__error}>Error</h1>
+      <h2 className={styles.Modal__title}>Books Didn't Load</h2>
+      <p className={styles.Modal__description}>
         This might have happened because you don't have internet or the server
         is down. Contact the system administrator or retry.
       </p>
-      <button class="Modal__RetryButton">Retry</button>
+      <button className={styles.Modal__retryButton}>Retry</button>
     </Modal>
   );
 };
