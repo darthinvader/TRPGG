ddc90132e5dd88b3e5b58251ea6d55aa891f70ce
darthinvader <psaradakisj@gmail.com>
2021-09-13 17:22:18 +0300

    [refactor] Navbar SignIn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Switch>
-          <Route exact path="/">
-            <Link to="/Books">Books</Link>
-          </Route>
+          <Navbar />
+          <Route exact path="/"></Route>
           <Route exact path="/Books"></Route>
         </Switch>
       </Router>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.scss";
-
+import { FcGoogle } from "react-icons/fc";
+import { auth, signIn } from "../../services/firebase-config";
+import { useState } from "react";
 const Navbar = () => {
+  const [user, setUser] = useState(auth.currentUser);
+  auth.onAuthStateChanged((user) => {
+    setUser(user);
+  });
+
+  const userButton = user ? (
+    <button className={styles.userButton}>
+      <img src={user.photoURL} alt="user"></img>
+    </button>
+  ) : (
+    <button className={styles.userButton} onClick={signIn}>
+      Sign In <FcGoogle></FcGoogle>
+    </button>
+  );
+
   return (
     <div className={styles.Navbar}>
-      <div className={styles.NavbarContainer}></div>
       <ul className={styles.Categories}>
         <li className={styles.Category}>
           <Link className={styles.Link} to="/books">
@@ -21,6 +37,7 @@ const Navbar = () => {
             Sound Effects
           </Link>
         </li>
+        <li className={styles.Category}>{userButton}</li>
       </ul>
     </div>
   );
