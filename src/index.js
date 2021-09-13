ae02edd4c78e0537435780acc75bb912fdc04054
darthinvader <psaradakisj@gmail.com>
2021-09-13 15:54:13 +0300

    [feat] added firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { config } from "dotenv";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./sass/main.scss";
+import "./services/firebase-config";
 
 ReactDOM.render(
   <React.StrictMode>
