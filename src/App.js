6228edbec7fb98981eca9a0126b3e15e0cb2ae98
darthinvader <psaradakisj@gmail.com>
2021-09-14 20:17:14 +0300

    [fix] navbar above switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./components/Books/Books";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
+
         <Switch>
-          <Navbar />
           <Route exact path="/"></Route>
-          <Route exact path="/Books"></Route>
+          <Route exact path="/books">
+            <Books />
+          </Route>
         </Switch>
       </Router>
     </div>
