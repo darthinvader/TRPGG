95112553257af25c6c62f2c413b28b8bfb0a0997
darthinvader <psaradakisj@gmail.com>
2021-09-15 14:11:54 +0300

    [feat] Book Created

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { database } from "../../services/firebase-config";
 import { ref, onValue } from "firebase/database";
-// import styles from "./Books.module.scss";
+import BookCard from "./BookCard/BookCard";
+import BookPreloader from "./BookPreloader/BookPreloader";
+import BookInfo from "./BookInfo/BookInfo";
+import styles from "./Books.module.scss";
 
 const Books = () => {
   const [books, setBooks] = useState(undefined);
@@ -12,8 +15,22 @@ const Books = () => {
       setBooks(data);
     });
   }, []);
+  console.log(books);
+  let bookElements = null;
+  if (!books)
+    bookElements = new Array(35).fill(0).map((_, index) => (
+      <BookCard key={index}>
+        <BookPreloader />
+      </BookCard>
+    ));
+  else
+    bookElements = books.map((book) => (
+      <BookCard key={book.title}>
+        <BookInfo book={book} />
+      </BookCard>
+    ));
 
-  return <div></div>;
+  return <div className={styles.BookCardsContainer}>{bookElements}</div>;
 };
 
 export default Books;
