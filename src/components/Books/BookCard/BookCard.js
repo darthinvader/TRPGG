cc7ef446cb368b3ee7efda325d88baedc05884e5
darthinvader <psaradakisj@gmail.com>
2021-09-15 13:49:46 +0300

    [fix] Bookcard returns

--- src/components/Books/BookCard/BookCard.js
+++ src/components/Books/BookCard/BookCard.js
@@ -1,6 +1,5 @@
 import styles from "./BookCard.module.scss";
-const BookCard = ({ children }) => {
-  <div className={styles.BookCard}>{children}</div>;
-};
-
+const BookCard = ({ children }) => (
+  <div className={styles.BookCard}>{children}</div>
+);
 export default BookCard;
