3858ccd064ff8b5d8f181390dae62b6d7ddfb932
darthinvader <psaradakisj@gmail.com>
2021-09-16 17:09:16 +0300

    [refactor] login

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,8 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.scss";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { RiLoader3Fill } from "react-icons/ri";
-import { auth, signIn, signOut } from "../../services/firebase-config";
-import { useEffect, useState } from "react";
+
+import Login from "./Login/Login";
 const Navbar = () => {
-  const [user, setUser] = useState(auth.currentUser);
-  const SignIn = () => {
-    setUser("Loading");
-    signIn();
-  };
-
-  useEffect(() => {
-    auth.onAuthStateChanged((currentUser) => {
-      setUser(currentUser);
-    });
-  }, []);
-
-  let userButton = (
-    <button className={styles.UserButton} onClick={SignIn}>
-      Sign In With <AiFillGoogleCircle className={styles.Icon} />
-    </button>
-  );
-
-  if (user) {
-    userButton =
-      user === "Loading" ? (
-        <div className={styles.UserButton}>
-          <RiLoader3Fill className={styles.Loading} />
-        </div>
-      ) : (
-        <button className={styles.userButton} onClick={signOut}>
-          <img className={styles.Image} src={user.photoURL} alt="user"></img>
-        </button>
-      );
-  }
-
   return (
     <div className={styles.Navbar}>
       <ul className={styles.Categories}>
@@ -56,7 +23,9 @@ const Navbar = () => {
         </li>
       </ul>
       <ul style={{ listStyleType: "none" }}>
-        <li className={styles.UserLi}>{userButton}</li>
+        <li className={styles.UserLi}>
+          <Login />
+        </li>
       </ul>
     </div>
   );
