ce59b9d3388247f714f8d0e14b900f1d68f26ca1
darthinvader <psaradakisj@gmail.com>
2021-09-17 17:55:43 +0300

    [feat] generators basics

---
+++ src/components/Generators/NameGenerators/NameGenerators.js
@@ -0,0 +1,12 @@
+const NameGenerators = () => {
+  const categories = [
+    "Fantasy & Folklore",
+    "Real Names",
+    "Places & Locations",
+    "Other Names",
+    "Description Generators",
+    "Other Gens",
+  ];
+};
+
+export default NameGenerators;
