909a5e46f54d8b8dd8bd68448bc4bce0f07dfb13
darthinvader <psaradakisj@gmail.com>
2021-09-17 17:56:22 +0300

    [feat] generators navLine

--- src/components/Navbar/NavLine/NavLine.js
+++ src/components/Navbar/NavLine/NavLine.js
@@ -2,10 +2,12 @@ import { useLocation } from "react-router";
 import styles from "./NavLine.module.scss";
 
 const NavLine = () => {
-  const { pathname } = useLocation();
+  let { pathname } = useLocation();
+  pathname = pathname.toLowerCase();
   if (pathname.includes("books")) {
     return <div className={styles.Books}></div>;
-  }
+  } else if (pathname.includes("generators"))
+    return <div className={styles.Generators}></div>;
   return null;
 };
 
