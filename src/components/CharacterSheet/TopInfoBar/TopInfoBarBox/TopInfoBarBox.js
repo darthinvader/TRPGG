44264c352ef8873284ecf0c7b31d3661d8e5403a
darthinvader <psaradakisj@gmail.com>
2021-09-19 17:00:59 +0300

    [feat] Character Sheet TopInfoBarBox css

--- src/components/CharacterSheet/TopInfoBar/TopInfoBarBox/TopInfoBarBox.js
+++ src/components/CharacterSheet/TopInfoBar/TopInfoBarBox/TopInfoBarBox.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import styles from "./TopInfoBarBox.module.scss";
 
 const TopInfoBarBox = ({ title, text }) => {
   const [textInfo, setTextInfo] = useState(text);
   return (
-    <div>
-      <span>{title}</span>
+    <div className={styles.Container}>
       <input
+        className={styles.Input}
         type="text"
         value={textInfo}
         onChange={(ev) => setTextInfo(ev.target.value)}
+        placeholder={title}
       ></input>
+      <span className={styles.Title}>{title}</span>
     </div>
   );
 };
