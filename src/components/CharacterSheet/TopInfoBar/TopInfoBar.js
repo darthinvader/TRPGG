150aabc500d23b23107f28916356bbf4cfa6f566
darthinvader <psaradakisj@gmail.com>
2021-09-21 20:03:56 +0300

    [refactor] Info Boxes

--- src/components/CharacterSheet/TopInfoBar/TopInfoBar.js
+++ src/components/CharacterSheet/TopInfoBar/TopInfoBar.js
@@ -1,10 +1,23 @@
-import TopInfoBarBox from "./TopInfoBarBox/TopInfoBarBox";
 import styles from "./TopInfoBar.module.scss";
+import InfoInput from "./TopInfoBarBox/InfoInput/InfoInput";
 
 const TopInfoBar = () => {
   return (
     <div className={styles.Container}>
-      <TopInfoBarBox title="Character Name" />
+      <div className={styles.Basics}>
+        <InfoInput placeholder="Character Name" fontSize="2.2rem" />
+        <InfoInput placeholder="Class & Level" fontSize="2.2rem" />
+        <InfoInput placeholder="Race" fontSize="1.8rem" />
+      </div>
+      <div className={styles.Secondary}>
+        <InfoInput placeholder="Age" fontSize="1.8rem" />
+        <InfoInput placeholder="Gender" fontSize="1.8rem" />
+        <InfoInput placeholder="Height" fontSize="1.8rem" />
+        <InfoInput placeholder="Weight" fontSize="1.8rem" />
+        <InfoInput placeholder="Eyes" fontSize="1.8rem" />
+        <InfoInput placeholder="Skin" fontSize="1.8rem" />
+        <InfoInput placeholder="Hair" fontSize="1.8rem" />
+      </div>
     </div>
   );
 };
