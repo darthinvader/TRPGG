00c9b01f9eec32350c123495e0a78af8fa8c0b72
darthinvader <psaradakisj@gmail.com>
2021-09-21 18:28:23 +0300

    [Feat] lock input

--- src/components/CharacterSheet/TopInfoBar/TopInfoBarBox/InfoInput/InfoInput.js
+++ src/components/CharacterSheet/TopInfoBar/TopInfoBarBox/InfoInput/InfoInput.js
@@ -1,21 +1,42 @@
 import { useState } from "react";
 import styles from "./InfoInput.module.scss";
-
+import { ImUnlocked, ImLock } from "react-icons/im";
 const InfoInput = ({ placeholder, input, fontSize }) => {
   const [text, setText] = useState(input || "");
+  const [lock, setLock] = useState(true);
   const textWidth = text.length + 2;
   const placeholderWidth = placeholder.length + 2;
   const size = textWidth > placeholderWidth ? textWidth : placeholderWidth;
   return (
-    <input
-      className={styles.Input}
-      type="text"
-      value={text}
-      onChange={(ev) => setText(ev.target.value)}
-      placeholder={placeholder}
-      size={size}
-      style={{ fontSize }}
-    ></input>
+    <div className={styles.Container}>
+      <input
+        className={styles.Input}
+        type="text"
+        value={text}
+        onChange={(ev) => setText(ev.target.value)}
+        placeholder={placeholder}
+        size={size}
+        style={{ fontSize }}
+        disabled={lock}
+      ></input>
+      {lock ? (
+        <ImLock
+          style={{ fontSize }}
+          className={styles.Lock}
+          onClick={() => {
+            setLock(!lock);
+          }}
+        />
+      ) : (
+        <ImUnlocked
+          style={{ fontSize }}
+          className={styles.Lock}
+          onClick={() => {
+            setLock(!lock);
+          }}
+        />
+      )}
+    </div>
   );
 };
 
