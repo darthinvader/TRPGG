c5deea1a3734eb6f48c7f6350f17ac123553e73d
darthinvader <psaradakisj@gmail.com>
2021-09-22 16:27:23 +0300

    [feat] added close button

--- src/components/FloatingInput/FloatingInput.js
+++ src/components/FloatingInput/FloatingInput.js
@@ -1,12 +1,14 @@
 import styles from "./FloatingInput.module.scss";
+import { MdClose } from "react-icons/md";
 
-const FloatingInput = ({ x, y, onSubmit }) => {
+const FloatingInput = ({ x, y, onSubmit, onClose }) => {
   return (
     <div className={styles.Container}>
       <input placeholder="Add Image Url" className={styles.Input}></input>
       <button className={styles.Button} onClick={onSubmit}>
         Add Image
       </button>
+      <MdClose />
     </div>
   );
 };
