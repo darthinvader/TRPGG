581c62674efc4d95b6c1b2e348745a9cb8746d63
darthinvader <psaradakisj@gmail.com>
2021-10-06 20:28:57 +0300

    [feat] theme light

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 import Books from "./components/Books/Books";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <Navbar />
       <Books />
-    </div>
+    </ThemeProvider>
   );
 }
 
