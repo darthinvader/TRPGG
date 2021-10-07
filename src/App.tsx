b48da1a216c0a65d1c5bf7fb928df4289464a4a2
darthinvader <psaradakisj@gmail.com>
2021-10-07 15:13:05 +0300

    [feat] added both light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { ThemeProvider } from "@emotion/react";
-import { createTheme, CssBaseline } from "@mui/material";
+import { createTheme, CssBaseline, darkScrollbar } from "@mui/material";
 import Books from "./components/Books/Books";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
   const theme = createTheme({
     palette: {
       mode: "dark",
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: darkScrollbar(),
+        },
+      },
     },
   });
 
@@ -16,7 +28,6 @@ function App() {
       <ThemeProvider theme={theme}>
         <Navbar />
         <CssBaseline />
-
         <Switch>
           <Route exact path="/"></Route>
           <Route exact path="/books">
