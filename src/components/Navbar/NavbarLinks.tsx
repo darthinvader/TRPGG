3691990ee1268346d3ac3dedcf6a4b638a1997bd
darthinvader <psaradakisj@gmail.com>
2021-10-08 17:57:17 +0300

    [feat] underline in current path

--- src/components/Navbar/NavbarLinks.tsx
+++ src/components/Navbar/NavbarLinks.tsx
@@ -1,6 +1,7 @@
 import { ListItem, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 
 interface Links {
   links: { link: string; title: string }[];
@@ -8,12 +9,19 @@ interface Links {
 
 const NavbarLinks: React.FC<Links> = ({ links }) => {
   const theme = useTheme();
+  const { pathname } = useLocation();
+
   const linkElements = links.map((link) => (
     <ListItem key={link.link}>
       <Link
         to={link.link}
         style={{
           textDecoration: "none",
+          color: "white",
+          borderBottom: pathname.includes(link.link)
+            ? `1px solid ${theme.custom.text}`
+            : "none",
+          paddingBottom: 2,
         }}
       >
         <Typography
