f536284e0815c80148f4cbb7ea3ee86f2e436d6e
darthinvader <psaradakisj@gmail.com>
2021-10-08 15:56:01 +0300

    [feat] navbar links now change color when hover

--- src/components/Navbar/NavbarLinks.tsx
+++ src/components/Navbar/NavbarLinks.tsx
@@ -1,26 +1,33 @@
-import { ListItem } from "@mui/material";
+import { ListItem, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 
 interface Links {
   links: { link: string; title: string }[];
 }
 
 const NavbarLinks: React.FC<Links> = ({ links }) => {
-  const theme: any = useTheme(); //TODO change theme from any to theme (right now theme doesn't work because interface is empty)
+  const theme = useTheme(); //TODO change theme from any to theme (right now theme doesn't work because interface is empty)
   console.log(theme);
   const linkElements = links.map((link) => (
-    <ListItem key={link.link} component={motion.p}>
+    <ListItem key={link.link}>
       <Link
         to={link.link}
         style={{
-          color: theme.custom.text,
-          fontSize: 20,
           textDecoration: "none",
         }}
       >
-        {link.title}
+        <Typography
+          sx={{
+            color: theme.custom.text,
+            fontSize: 20,
+            "&:hover": {
+              color: theme.palette.text.primary,
+            },
+          }}
+        >
+          {link.title}
+        </Typography>
       </Link>
     </ListItem>
   ));
