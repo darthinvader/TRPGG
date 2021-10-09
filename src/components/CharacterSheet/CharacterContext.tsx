81a0df3585a11d736559430493aa87caa40ed6aa
darthinvader <psaradakisj@gmail.com>
2021-10-09 13:29:47 +0300

    [feat] basic context

---
+++ src/components/CharacterSheet/CharacterContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, FC, useContext, useState } from "react";
+
+const CharacterContext = createContext<any>(null);
+const CharacterUpdateContext = createContext<any>(null);
+
+export const useCharacter = () => useContext(CharacterContext);
+export const useCharacterUpdate = () => useContext(CharacterUpdateContext);
+
+const CharacterProvider: FC<{ children?: React.ReactNode }> = ({
+  children,
+}) => {
+  const [character, setCharacter] = useState<any>({});
+  return (
+    <CharacterContext.Provider value={character}>
+      <CharacterUpdateContext.Provider value={"hmm"}>
+        {children}
+      </CharacterUpdateContext.Provider>
+    </CharacterContext.Provider>
+  );
+};
+
+export default CharacterProvider;
