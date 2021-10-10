3059b515748f025c562fb85cd760707b6a9e20a9
darthinvader <psaradakisj@gmail.com>
2021-10-10 19:14:24 +0300

    [feat] race misc

---
+++ src/interfaces/raceMisc.tsx
@@ -0,0 +1,9 @@
+enum RaceMisc {
+  BaseRace = "Base Race",
+  HasImages = "Has Images",
+  HasInfo = "Has Info",
+  KeyRace = "Key Race",
+  Lineage = "Lineage",
+  ModifiedCopy = "Modified Copy",
+  SRD = "SRD",
+}
