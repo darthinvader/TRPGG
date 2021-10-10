5d21e41011c40f1aca2141c103cef907836417d2
darthinvader <psaradakisj@gmail.com>
2021-10-10 18:07:47 +0300

    [feat] Ability bonuses

---
+++ src/interfaces/abilityBonuses.tsx
@@ -0,0 +1,10 @@
+interface AbilityBonuses {
+  Strengh: number;
+  Dexterity: number;
+  Constitution: number;
+  Intelligence: number;
+  Wisdom: number;
+  Charisma: number;
+}
+
+export default AbilityBonuses;
