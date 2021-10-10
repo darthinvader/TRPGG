b8be9987dab687fae64c57a035816f6a4d1b456f
darthinvader <psaradakisj@gmail.com>
2021-10-10 18:34:42 +0300

    [feat] trait

---
+++ src/interfaces/trait.tsx
@@ -0,0 +1,27 @@
+enum RaceTrait {
+  Amphibious = "Amphibious ",
+  ArmorProficiency = "Armor Proficiency",
+  Blindsight = "Blindsight",
+  ConditionImmunity = "Condition Immunity",
+  DamageImmunity = "Damage Immunity",
+  DamageResistance = "DamageResistance",
+  Darkvision = "Darkvision",
+  Dragonmark = "Dragonmark",
+  Feat = "Feat",
+  ImprovedResting = "Improved Resting",
+  MagicResistance = "Magic Resistance",
+  MonstrousRace = "Monstrous Race",
+  NPCRace = "NPC Race",
+  NaturalArmor = "Natural Armor",
+  PowerfulBuild = "Powerful Build",
+  SkillProficiency = "Skill Proficiency",
+  Spellcasting = "Spellcasting",
+  SunlightSensitivity = "Sunlight Sensitivity",
+  SuperiorDarkvision = "Superior Darkvision",
+  ToolProficiency = "Tool Proficiency",
+  UnarmedStrike = "Unarmed Strike",
+  UncommonRace = "Uncommon Race",
+  WeaponProficiency = "Weapon Proficiency",
+}
+
+export default RaceTrait;
