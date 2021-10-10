47f211c426389cccdb4eb65ebc14bb0a2f77ab15
darthinvader <psaradakisj@gmail.com>
2021-10-10 19:10:44 +0300

    [feat] creature type

---
+++ src/interfaces/creatureType.tsx
@@ -0,0 +1,18 @@
+enum CreatureType {
+  Aberration = "Aberration",
+  Beast = "Beast",
+  Celestial = "Celestial",
+  Construct = "Construct",
+  Dragon = "Dragon",
+  Elemental = "Elemental",
+  Fey = "Fey",
+  Fiend = "Fiend",
+  Giant = "Giant",
+  Humanoid = "Humanoid",
+  Monstrosity = "Monstrosity",
+  Ooze = "Ooze",
+  Plant = "Plant",
+  Undead = "Undead",
+}
+
+export default CreatureType;
