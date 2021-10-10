e107e9909bc8d8b87e0299191d26e1d2cb869f28
darthinvader <psaradakisj@gmail.com>
2021-10-10 12:00:09 +0300

    [feat] sizes enum

---
+++ src/interfaces/size.tsx
@@ -0,0 +1,10 @@
+enum Sizes {
+  Tiny = "Tiny",
+  Small = "Small",
+  Medium = "Medium",
+  Large = "Large",
+  Huge = "Huge",
+  Gargantuan = "Gargantuan",
+  Varies = "Varies",
+}
+export default Sizes;
