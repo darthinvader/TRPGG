be63f07c6dd59c4363538a3a76d68018dc3bd0dc
darthinvader <psaradakisj@gmail.com>
2021-10-10 16:36:53 +0300

    [feat] movenent types

---
+++ src/interfaces/movementType.tsx
@@ -0,0 +1,11 @@
+enum MovementType {
+  Burrow = "Burrow",
+  Climb = "Climb",
+  Fly = "Fly",
+  Swim = "Swim",
+  Walk = "Walk",
+  WalkSlow = "Walk (Slow)",
+  WalkFast = "Walk (Fast)",
+}
+
+export default MovementType;
