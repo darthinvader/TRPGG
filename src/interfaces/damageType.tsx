cda9e7f7dbca15775bf9f24cfb3b0ca76083015e
darthinvader <psaradakisj@gmail.com>
2021-10-14 16:52:29 +0300

    [feat] damage type

---
+++ src/interfaces/damageType.tsx
@@ -0,0 +1,17 @@
+enum DamageType {
+  Acid = "acid",
+  Bludgeoning = "bludgeoning",
+  Cold = "cold",
+  Fire = "fire",
+  Force = "force",
+  Lightning = "lightning",
+  Necrotic = "necrotic",
+  Piercing = "piercing",
+  Poison = "poison",
+  Psychic = "psychic",
+  Radiant = "radiant",
+  Slashing = "slashing",
+  Thunder = "thunder",
+}
+
+export default DamageType;
