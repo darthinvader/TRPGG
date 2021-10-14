897ac5f83746377f0fa3f7120cacb46ffbd1db59
darthinvader <psaradakisj@gmail.com>
2021-10-14 13:15:29 +0300

    [feat] Schools

---
+++ src/interfaces/spellSchool.tsx
@@ -0,0 +1,22 @@
+enum SpellSchool {
+  A = "Abjuration",
+  V = "Evocation",
+  E = "Enchantment",
+  I = "Illusion",
+  D = "Divination",
+  N = "Necormanc",
+  T = "Transmutation",
+  C = "Conjuration",
+  P = "Psionics",
+  Abjuration = "Abjuration",
+  Evocation = "Evocation",
+  Enchantment = "Enchantment",
+  Illusion = "Illusion",
+  Divination = "Divination",
+  Necormancy = "Necormancy",
+  Transmutation = "Transmutation",
+  Conjuration = "Conjuration",
+  Psionics = "Psionics",
+}
+
+export default SpellSchool;
