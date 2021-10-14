0fab8e855c3d55c45c1d7a94fad77bfd18b722af
darthinvader <psaradakisj@gmail.com>
2021-10-14 17:15:41 +0300

    [feat] misc Spell Tag

---
+++ src/interfaces/miscSpellTag.tsx
@@ -0,0 +1,11 @@
+enum MiscSpellTag {
+  PRM = "Permanent Effect",
+  SCL = "Scaling Effect",
+  HL = "Heals",
+  SMN = "Summons",
+  SGT = "Requires Sight",
+  THP = "Temporary HP",
+  MAC = "Modifies AC",
+  TP = "Teleport",
+  FMV = "Forces Movement",
+}
