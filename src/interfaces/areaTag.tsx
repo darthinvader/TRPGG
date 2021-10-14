0076288a2ddebfceb85b59ac4b9443d6242cc26e
darthinvader <psaradakisj@gmail.com>
2021-10-14 17:11:52 +0300

    [feat] areaTag

---
+++ src/interfaces/areaTag.tsx
@@ -0,0 +1,15 @@
+enum AreaTag {
+  ST = "Single Target",
+  MT = "Multi Target",
+  R = "Circle",
+  N = "Cone",
+  C = "Cube",
+  Y = "Cylinder",
+  H = "Hemisphere",
+  L = "Line",
+  S = "Sphere",
+  Q = "Square",
+  W = "Wall",
+}
+
+export default AreaTag;
