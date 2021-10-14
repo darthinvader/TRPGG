f1212e72e2653f514626e1bdffc11039b5d606a1
darthinvader <psaradakisj@gmail.com>
2021-10-14 17:00:36 +0300

    [feat] Ability

---
+++ src/interfaces/ability.tsx
@@ -0,0 +1,10 @@
+enum Ability {
+  Strength = "strength",
+  Constitution = "constitution",
+  Dexterity = "dexterity",
+  Intelligence = "intelligence",
+  Wisdom = "wisdom",
+  Charisma = "charisma",
+}
+
+export default Ability;
