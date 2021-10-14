c3dba5209bd9c6f8d7a48222579b84e6ad2bfffc
darthinvader <psaradakisj@gmail.com>
2021-10-14 16:56:09 +0300

    [feat] Condition

---
+++ src/interfaces/condition.tsx
@@ -0,0 +1,20 @@
+enum Condition {
+  Blinded = "blinded",
+  Charmed = "charmed",
+  Deafened = "deafened",
+  Exhaustion = "exhaustion",
+  Frightened = "frightened",
+  Grappled = "grappled",
+  Incapacitated = "incapacitated",
+  Invisible = "invisible",
+  Paralyzed = "paralyzed",
+  Petrified = "petrified",
+  Poisoned = "poisoned",
+  Prone = "prone",
+  Restrained = "restrained",
+  Stunned = "stunned",
+  Unconscious = "unconscious",
+}
+
+export default Condition;
+  
