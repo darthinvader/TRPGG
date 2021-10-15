e3664c85ab76c982493c53480d5a9749219f46cc
darthinvader <psaradakisj@gmail.com>
2021-10-15 19:26:41 +0300

    [feat] skills

---
+++ src/interfaces/utils/skill.tsx
@@ -0,0 +1,22 @@
+enum Skill {
+  Athletics = "athletics",
+  Acrobatics = "acrobatics",
+  SleightOfHand = "sleight of hand",
+  Stealth = "stealth",
+  Arcana = "arcana",
+  History = "history",
+  Investigation = "investigation",
+  Nature = "nature",
+  Religion = "religion",
+  AnimalHandling = "animal handling",
+  Insight = "insight",
+  Medicine = "medicine",
+  Perception = "perception",
+  Survival = "survival",
+  Deception = "deception",
+  Intimidation = "intimidation",
+  Performance = "performance",
+  Persuasion = "persuasion",
+}
+
+export default Skill;
