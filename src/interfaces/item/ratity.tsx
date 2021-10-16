418e7f4982deb60972e7e8f93c5e899a6302f2f3
darthinvader <psaradakisj@gmail.com>
2021-10-16 17:18:08 +0300

    [feat] rarity

---
+++ src/interfaces/item/ratity.tsx
@@ -0,0 +1,14 @@
+enum Rarity {
+  Rare = "rare",
+  None = "none",
+  Uncommon = "uncommon",
+  VeryRare = "very rare",
+  Legendary = "legendary",
+  Artifact = "artifact",
+  Unknown = "unknown",
+  Common = "common",
+  UnknownMagic = "unknown (magic)",
+  Varies = "varies",
+}
+
+export default Rarity;
