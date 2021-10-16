ad540d76df123804a8e08dbac81ff2438b1fb4dc
darthinvader <psaradakisj@gmail.com>
2021-10-16 15:41:40 +0300

    [feat] Patron

---
+++ src/interfaces/utils/patron.tsx
@@ -0,0 +1,10 @@
+enum Patron {
+  Archfey = "The Archfey",
+  Fiend = "The Fiend",
+  GreatOldONe = "The Great Old One",
+  Hexblade = "The Hexblade",
+  RavenQueen = "The Raven Queen",
+  Seeker = "The Seeker",
+}
+
+export default Patron;
