7472d377fd00db7387bc26f5d17353d2988b41db
darthinvader <psaradakisj@gmail.com>
2021-10-16 18:01:40 +0300

    [feat] Property

---
+++ src/interfaces/item/property.tsx
@@ -0,0 +1,16 @@
+enum Property {
+  T = "Thrown",
+  V = "Versatile",
+  H = "Heavy",
+  TH = "Two-Handed",
+  F = "Finesse",
+  L = "Light",
+  R = "Reach",
+  A = "Ammunition",
+  LD = "Loading",
+  S = "Special",
+  RLD = "Reload",
+  BF = "Burst Fire",
+}
+
+export default Property;
