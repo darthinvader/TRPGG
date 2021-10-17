5e233ad20cc51e2db269f663d7bd9d36d5aff0b7
darthinvader <psaradakisj@gmail.com>
2021-10-17 13:56:58 +0300

    [feat] recharge time

---
+++ src/interfaces/item/rechargeTime.tsx
@@ -0,0 +1,11 @@
+enum RechargeTime {
+  Round = "round",
+  RestShort = "restShort",
+  RestLong = "restLong",
+  Dawn = "dawn",
+  Dusk = "dusk",
+  Midnight = "midnight",
+  Special = "special",
+}
+
+export default RechargeTime;
