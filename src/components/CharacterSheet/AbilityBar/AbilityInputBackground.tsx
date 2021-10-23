eb91f97562699630d6dd47345aeda7b4b716dca3
darthinvader <psaradakisj@gmail.com>
2021-10-23 14:27:56 +0300

    [feat] stroke and fill svgs

--- src/components/CharacterSheet/AbilityBar/AbilityInputBackground.tsx
+++ src/components/CharacterSheet/AbilityBar/AbilityInputBackground.tsx
@@ -1,13 +1,24 @@
 import { useTheme } from "@mui/material";
-import { ReactComponent as AbilityVector } from "./svgs/ability.svg";
-import { ReactComponent as AbilityDarkModeVector } from "./svgs/abilityDarkMode.svg";
+import { ReactComponent as Ability } from "./svgs/Ability.svg";
 
 const AbilityInputBackground = () => {
   const theme = useTheme();
   if (theme.palette.mode === "light") {
-    return <AbilityVector style={{ height: 125, width: 125 }} />;
+    return (
+      <Ability
+        fill="transparent"
+        stroke="blue"
+        style={{ height: 125, width: 125 }}
+      />
+    );
   } else {
-    return <AbilityDarkModeVector style={{ height: 125, width: 125 }} />;
+    return (
+      <Ability
+        fill="transparent"
+        stroke="yellow"
+        style={{ height: 125, width: 125 }}
+      />
+    );
   }
 };
 
