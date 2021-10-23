25d01aff0466057b2ac09749859780a1515a5b4a
darthinvader <psaradakisj@gmail.com>
2021-10-23 16:54:13 +0300

    [feat] Classes enums

---
+++ src/interfaces/utils/classes.tsx
@@ -0,0 +1,19 @@
+enum Classes {
+  Artificer = "Artificer",
+  Barbarian = "Barbarian",
+  Bard = "Bard",
+  BloodHunter = "Blood Hunter",
+  Cleric = "Cleric",
+  Druid = "Druid",
+  Fighter = "Fighter",
+  Monk = "Monk",
+  Paladin = "Paladin",
+  Ranger = "Ranger",
+  Rogue = "Rogue",
+  Shaman = "Shaman",
+  Sorcerer = "Sorcerer",
+  Warlock = "Warlock",
+  Wizard = "Wizard",
+}
+
+export default Classes;
