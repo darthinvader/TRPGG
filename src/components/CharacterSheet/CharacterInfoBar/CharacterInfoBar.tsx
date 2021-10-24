de0da16bcf8d8926763f708fc1c39a9f379615a7
darthinvader <psaradakisj@gmail.com>
2021-10-24 13:58:48 +0300

    [refactor] Inputs

--- src/components/CharacterSheet/CharacterInfoBar/CharacterInfoBar.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/CharacterInfoBar.tsx
@@ -1,11 +1,153 @@
+import BareInput from "../../../utilComponents/BareInput";
 import CharacterImage from "./CharacterImage";
-import CharacterInfo from "./CharacterInfo";
+import ClassAndLevelContainer from "./ClassAndLevelContainer";
 
 const CharacterInfoBar = () => {
   return (
     <div style={{ display: "flex", alignItems: "center", gap: 16 }}>
       <CharacterImage imageUrl="https://i.pinimg.com/236x/cd/93/06/cd9306aa0acf3e3ffc55443d99600b7f.jpg" />
-      <CharacterInfo />
+      <BareInput
+        variant="standard"
+        label="Character Name"
+        inputProps={{
+          style: {
+            fontSize: 24,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 24,
+          },
+        }}
+      />
+
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 16,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 16,
+          },
+        }}
+        variant="standard"
+        label="Race"
+      />
+      <BareInput
+        variant="standard"
+        label="Background"
+        inputProps={{
+          style: {
+            fontSize: 14,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 14,
+          },
+        }}
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Alignment"
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Age"
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Eyes"
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Height"
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Skin"
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Weight"
+      />
+      <BareInput
+        inputProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            fontSize: 12,
+          },
+        }}
+        variant="standard"
+        label="Hair"
+      />
+      <ClassAndLevelContainer />
     </div>
   );
 };
--- src/components/CharacterSheet/CharacterInfoBar/ClassAndLevelContainer.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/ClassAndLevelContainer.tsx
@@ -1,24 +1,24 @@
 import { Button } from "@mui/material";
 import ClassAndLevel from "./ClassAndLevel";
+import Paper from "@mui/material/Paper";
 
 const ClassAndLevelContainer = () => {
   return (
-    <span
-      style={{
+    <Paper
+      sx={{
         display: "flex",
         flexDirection: "column",
         overflowY: "auto",
         height: 124,
-        border: "1px solid white",
         padding: "8px 16px",
-        gap: 8,
+        gap: 2,
       }}
     >
       <ClassAndLevel />
       <ClassAndLevel />
       <ClassAndLevel />
       <Button>Add New Class</Button>
-    </span>
+    </Paper>
   );
 };
 
