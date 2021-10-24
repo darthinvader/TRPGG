6fb09f37f371faf865c0a2b0afcc9f5727bc2e94
darthinvader <psaradakisj@gmail.com>
2021-10-24 19:26:06 +0300

    [feat] character image now can change image

--- src/components/CharacterSheet/CharacterInfoBar/CharacterImage.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/CharacterImage.tsx
@@ -1,21 +1,93 @@
-import { Avatar } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useCharacter, useCharacterUpdate } from "../CharacterContext";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { useState } from "react";
+const CharacterImage = () => {
+  const [open, setOpen] = useState(false);
+  const [imageUrlValue, setImageUrlValue] = useState("");
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-interface Props {
-  imageUrl: string;
-}
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-const CharacterImage: React.FC<Props> = ({ imageUrl }) => {
+  const character = useCharacter();
+  const imageUrl = character.imageUrl;
+  console.log(imageUrl);
+
+  const { changeInfo } = useCharacterUpdate();
+
+  let image: JSX.Element;
+  if (imageUrl === "") {
+    image = (
+      <Button sx={{ border: `2px solid blue` }} onClick={handleOpen}>
+        <AddPhotoAlternateIcon sx={{ width: 100, height: 100 }} />
+      </Button>
+    );
+  } else {
+    image = (
+      <Avatar
+        variant="rounded"
+        alt="Character Image"
+        src={imageUrl}
+        sx={{
+          height: 100,
+          width: 100,
+          border: `2px solid blue`,
+          cursor: "pointer",
+        }}
+        onClick={handleOpen}
+      />
+    );
+  }
   return (
-    <Avatar
-      variant="rounded"
-      alt="Character Image"
-      src={imageUrl}
-      sx={{
-        height: 70,
-        width: 70,
-        border: `2px solid blue`,
-      }}
-    />
+    <>
+      {image}
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Add the image of your character. You must add a url. If the link
+            does not work a default image will appear. You can always change the
+            image later.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="imageUrl"
+            label="Image Url"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={imageUrlValue}
+            onChange={(e) => setImageUrlValue(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            onClick={() => {
+              changeInfo("imageurl", imageUrlValue);
+              handleClose();
+            }}
+          >
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 export default CharacterImage;
