c0eed1ae77f747d080e00c32056d068e21750ae7
darthinvader <psaradakisj@gmail.com>
2021-10-26 15:52:48 +0300

    [feat] character Info Enum

---
+++ src/interfaces/character/characterInfo.tsx
@@ -0,0 +1,14 @@
+enum CharacterInfo {
+  ImageUrl = "imageUrl",
+  Name = "name",
+  Race = "race",
+  Background = "background",
+  Age = "age",
+  eyes = "eyes",
+  height = "height",
+  skin = "skin",
+  weight = "weight",
+  hair = "hair",
+}
+
+export default CharacterInfo;
