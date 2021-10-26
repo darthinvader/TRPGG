06ad8c391d24248c3f1cc57e171058d7ca6eeafc
darthinvader <psaradakisj@gmail.com>
2021-10-26 16:00:14 +0300

    [feat] info utils

--- src/interfaces/character/characterUtils.tsx
+++ src/interfaces/character/characterUtils.tsx
@@ -1,6 +1,7 @@
 import Ability from "../utils/ability";
 import AbilityScore, { emptyAbilityScore } from "./abilityScore";
 import Character from "./character";
+import CharacterInfo from "./characterInfo";
 
 export const getAbility = (character: Character, abilityName: string) => {
   switch (abilityName.toLowerCase()) {
@@ -45,6 +46,69 @@ export const setAbility = (
       break;
     case Ability.Charisma.toLowerCase():
       character.charisma = value;
+  }
+  return character;
+};
+
+export const getInfo = (character: Character, infoName: string) => {
+  switch (infoName.toLowerCase()) {
+    case CharacterInfo.Age.toLowerCase():
+      return character.age;
+    case CharacterInfo.Background.toLowerCase():
+      return character.background;
+    case CharacterInfo.Name.toLowerCase():
+      return character.name;
+    case CharacterInfo.Race.toLowerCase():
+      return character.race;
+    case CharacterInfo.Eyes.toLowerCase():
+      return character.eyes;
+    case CharacterInfo.Hair.toLowerCase():
+      return character.hair;
+    case CharacterInfo.Skin.toLowerCase():
+      return character.skin;
+    case CharacterInfo.Height.toLowerCase():
+      return character.height;
+    case CharacterInfo.Weight.toLowerCase():
+      return character.weight;
+    default:
+      console.error("You get an ability Score that doesnt extist");
+      return "";
+  }
+};
+
+export const setInfo = (
+  character: Character,
+  infoName: string,
+  value: string
+) => {
+  switch (infoName.toLowerCase()) {
+    case CharacterInfo.Age.toLowerCase():
+      character.age = value;
+      break;
+    case CharacterInfo.Background.toLowerCase():
+      character.background = value;
+      break;
+    case CharacterInfo.Name.toLowerCase():
+      character.name = value;
+      break;
+    case CharacterInfo.Race.toLowerCase():
+      character.race = value;
+      break;
+    case CharacterInfo.Eyes.toLowerCase():
+      character.eyes = value;
+      break;
+    case CharacterInfo.Hair.toLowerCase():
+      character.hair = value;
+      break;
+    case CharacterInfo.Skin.toLowerCase():
+      character.skin = value;
+      break;
+    case CharacterInfo.Height.toLowerCase():
+      character.height = value;
+      break;
+    case CharacterInfo.Weight.toLowerCase():
+      character.weight = value;
+      break;
   }
   return character;
 };
