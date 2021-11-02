e78aa1929bca48524f67d00977223b4747f373d0
darthinvader <psaradakisj@gmail.com>
2021-11-02 18:17:40 +0100

    [feat] Basic Info

--- src/components/CharacterSheet/CharacterInfoBar/BasicInfo.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/BasicInfo.tsx
@@ -3,6 +3,10 @@ import { Typography } from "@mui/material";
 const BasicInfo = () => {
   return (
     <>
+      <Typography variant="h4">Character Name</Typography>
+      <Typography>Race</Typography>
+      <Typography>Class/XX | Class/XX | Class/XX</Typography>
+      <Typography>Level XX</Typography>
       <Typography>Character Name</Typography>
     </>
   );
