0c89147ad3d85515b229f1ed7f2a933a877ffa43
darthinvader <psaradakisj@gmail.com>
2021-11-05 16:03:17 +0100

    [feat] all the info for Secondary Info

--- src/components/CharacterSheet/CharacterInfoBar/SecondaryInfo.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/SecondaryInfo.tsx
@@ -4,6 +4,11 @@ const SecondaryInfo = () => {
   return (
     <>
       <Typography>Eyes</Typography>
+      <Typography>Hair</Typography>
+      <Typography>Skin</Typography>
+      <Typography>Age</Typography>
+      <Typography>Height</Typography>
+      <Typography>Weight</Typography>
     </>
   );
 };
