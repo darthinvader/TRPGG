76926ce9114abbd2bb6d05de5b97d04e7e6d8d14
darthinvader <psaradakisj@gmail.com>
2021-11-06 08:55:51 +0100

    [feat] Character Info Bar Components ready

--- src/components/CharacterSheet/CharacterInfoBar/CharacterInfoBar.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/CharacterInfoBar.tsx
@@ -1,5 +1,7 @@
 import BasicInfo from "./BasicInfo";
 import CharacterImage from "./CharacterImage";
+import SecondaryInfo from "./SecondaryInfo";
+import EditIcon from "@mui/icons-material/Edit";
 
 const CharacterInfoBar = () => {
   return (
@@ -7,6 +9,8 @@ const CharacterInfoBar = () => {
       <div>
         <CharacterImage />
         <BasicInfo />
+        <SecondaryInfo />
+        <EditIcon />
       </div>
     </div>
   );
