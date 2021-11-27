b3f2f23a97b8868dbd0a7a2b884d61ac0c2ecc24
darthinvader <psaradakisj@gmail.com>
2021-11-27 22:54:43 +0100

    [fix] unused props

--- src/components/CharacterSheet/CharacterInfoBar/SecondaryInfo.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/SecondaryInfo.tsx
@@ -4,17 +4,12 @@ import TooltipedTypography from "../../../utilComponents/TooltipedTypography";
 const SecondaryInfo = () => {
   return (
     <Box sx={{ display: "grid" }}>
-      <TooltipedTypography
-        title="Eyes"
-        tooltipProps={{ placement: "left", arrow: true }}
-      >
-        Eyes
-      </TooltipedTypography>
-      <TooltipedTypography title="Hair">Hair</TooltipedTypography>
-      <TooltipedTypography title="Skin">Skin</TooltipedTypography>
-      <TooltipedTypography title="Age">Age</TooltipedTypography>
-      <TooltipedTypography title="Height">Height</TooltipedTypography>
-      <TooltipedTypography title="Weight">Weight</TooltipedTypography>
+      <TooltipedTypography title="Eyes">Eyes:</TooltipedTypography>
+      <TooltipedTypography title="Hair">Hair:</TooltipedTypography>
+      <TooltipedTypography title="Skin">Skin:</TooltipedTypography>
+      <TooltipedTypography title="Age">Age:</TooltipedTypography>
+      <TooltipedTypography title="Height">Height:</TooltipedTypography>
+      <TooltipedTypography title="Weight">Weight:</TooltipedTypography>
     </Box>
   );
 };
