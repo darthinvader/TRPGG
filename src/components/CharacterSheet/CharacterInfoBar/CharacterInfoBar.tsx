373336241f285310799e67e3a9d6d4839abaa501
darthinvader <psaradakisj@gmail.com>
2021-11-27 22:55:27 +0100

    [feat] Long & Short Rest

--- src/components/CharacterSheet/CharacterInfoBar/CharacterInfoBar.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/CharacterInfoBar.tsx
@@ -2,17 +2,26 @@ import BasicInfo from "./BasicInfo";
 import CharacterImage from "../CharacterImage/CharacterImage";
 import SecondaryInfo from "./SecondaryInfo";
 import EditIcon from "@mui/icons-material/Edit";
-import { Card, IconButton } from "@mui/material";
-
+import { Button, Card, IconButton } from "@mui/material";
+import { GiCampfire, GiCampingTent } from "react-icons/gi";
+import { Box } from "@mui/system";
 const CharacterInfoBar = () => {
   return (
     <Card sx={{ display: "flex", alignItems: "center", padding: 2 }}>
       <CharacterImage />
       <BasicInfo />
       <SecondaryInfo />
-      <IconButton sx={{ flexShrink: 0 }}>
+      <IconButton>
         <EditIcon />
       </IconButton>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Button size="large" startIcon={<GiCampingTent />}>
+          Long rest
+        </Button>
+        <Button size="large" startIcon={<GiCampfire />}>
+          Short rest
+        </Button>
+      </Box>
     </Card>
   );
 };
