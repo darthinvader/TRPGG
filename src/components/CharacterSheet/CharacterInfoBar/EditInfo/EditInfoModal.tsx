0f99292ca848e245d37d1aeb1d4824cf060ed4ad
darthinvader <psaradakisj@gmail.com>
2021-11-28 22:21:05 +0100

    [refactor] new styling and TODOS

--- src/components/CharacterSheet/CharacterInfoBar/EditInfo/EditInfoModal.tsx
+++ src/components/CharacterSheet/CharacterInfoBar/EditInfo/EditInfoModal.tsx
@@ -6,29 +6,39 @@ interface Props {
 }
 
 const EditInfoModal: React.FC<Props> = ({ open, onClose }) => {
+  // TODO: Races need to be pulled from firebase
+  // TODO: Classes need to be pulled from firebase
+  // TODO: Backgrounds to be pulled from firebase
+  // TODO: Class & Level Need more ui (For multiclassing)
+  // TODO: Character Name may have a Generator for help
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Edit Character Information</DialogTitle>
-      <DialogContent
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexFlow: "wrap",
-        }}
-      >
-        <TextField label="Character Name" />
-        <TextField label="Race" />
-        <TextField label="Class" />
-        <TextField label="Level" />
-        <TextField label="Background" />
-        <TextField label="Alignment" />
-        <TextField label="Eyes" />
-        <TextField label="Hair" />
-        <TextField label="Skin" />
-        <TextField label="Age" />
-        <TextField label="Height" />
-        <TextField label="Weight" />
+      <DialogTitle sx={{ marginBottom: 3 }}>
+        Edit Character Information
+      </DialogTitle>
+      <DialogContent>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexFlow: "wrap",
+            gap: "16px",
+            marginTop: "24px",
+          }}
+        >
+          <TextField label="Character Name" />
+          <TextField label="Race" />
+          <TextField label="Class" />
+          <TextField label="Background" />
+          <TextField label="Alignment" />
+          <TextField label="Eyes" />
+          <TextField label="Hair" />
+          <TextField label="Skin" />
+          <TextField label="Age" />
+          <TextField label="Height" />
+          <TextField label="Weight" />
+        </div>
       </DialogContent>
     </Dialog>
   );
