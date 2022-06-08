babeb28faaacd52bc970e5790813502b98feb67d
darthinvader <psaradakisj@gmail.com>
2022-06-08 20:06:00 +0200

    [refactor] massive delete

--- src/components/Navbar/NavLine.tsx
+++
@@ -1,7 +0,0 @@
-import { yellow } from "@mui/material/colors";
-import { Box } from "@mui/system";
-const NavLine = () => {
-  return <Box sx={{ bgcolor: yellow[400], height: "1px", zIndex: 100 }}></Box>;
-};
-
-export default NavLine;
